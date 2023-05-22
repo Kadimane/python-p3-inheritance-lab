93d9328e858844f1abe3e6f1552faf87f78c89f1
Kadimane <kadimanegloriah@gmail.com>
2023-05-23 01:43:45 +0300

    complete assignment

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,39 @@ from user import User
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = ["str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",]
 
-    def teach(self):
-        pass
+    def teach (self):
+        if self.knowledge:
+            return self.knowledge[0]
+        else:
+            print("No information provided.Teaching cannot be performed")
+    
+teach = Teacher("Kadimane","Gloriah")
+teach.teach()
+print(teach.first_name,teach.last_name)
+print(teach.knowledge[4])
+
+        
+
+
+
+
+
+
+                   
+    
+
+
+        
+
+        
